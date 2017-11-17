a87310cbd887ea7ff8af9b6c609200608e76ff97
KIRILL-PC\Kirill <Ksardas-zkd@yandex.ru>
2017-11-17 21:43:30 +0300

    Исправил ещё один баг (при множественном нажатии неправильной кнопки)

--- src/components/ColorButton.js
+++ src/components/ColorButton.js
@@ -3,45 +3,61 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { addPlayerStep, continueOrRestart } from '../actions/sequenceAction'
 
-export const ColorButton = (props) => {
-
-    const lightClass = {
+export class ColorButton extends React.PureComponent {
+    constructor(props) {
+        super(props)
+        this.state = {
+            disabled: false
+        }
+    }
+    lightClass = {
         redButton: 'lightRedButton',
         greenButton: 'lightGreenButton',
         yellowButton: 'lightYellowButton',
         blueButton: 'lightBlueButton',
     }
 
-    const urlSound = {
+    urlSound = {
         redButton: require('../assets/sounds/simonSound1.mp3'),
         greenButton: require('../assets/sounds/simonSound2.mp3'),
         yellowButton: require('../assets/sounds/simonSound3.mp3'),
         blueButton: require('../assets/sounds/simonSound4.mp3'),
     }
 
-    const { id, playerSequence, computerSequence, lighten, lightButton, isFinish, finishBlink } = props
-    const { addPlayerStep, continueOrRestart } = props
-    const onBtnClick = () => {
+    undisable = async () => {
+        await setTimeout(() => { this.setState({ disabled: false }) }, 1500)
+    }
+
+
+    onBtnClick = () => {
+        const { id, playerSequence, computerSequence } = this.props
+        const { addPlayerStep, continueOrRestart } = this.props
         addPlayerStep(id)
         const buttonSound = new Audio
         if (computerSequence[playerSequence.length] === id)
-            buttonSound.src = urlSound[id]
-        else
+            buttonSound.src = this.urlSound[id]
+        else {
+            this.setState({ disabled: true })
+            this.undisable()
             buttonSound.src = require('../assets/sounds/wrong.mp3')
+        }
         buttonSound.play()
         continueOrRestart()
     }
 
-    return (
-        <div>
-            <button
-                className={`block-unit ${id} ${lighten && lightClass[lightButton]}`}
-                disabled={!finishBlink || isFinish}
-                id={id}
-                onClick={onBtnClick}
-            />
-        </div>
-    )
+    render() {
+        const { id, lighten, lightButton, isFinish, finishBlink } = this.props
+        return (
+            <div>
+                <button
+                    className={`block-unit ${id} ${lighten && this.lightClass[lightButton]}`}
+                    disabled={this.state.disabled || !finishBlink || isFinish}
+                    id={id}
+                    onClick={this.onBtnClick}
+                />
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = (state, ownProps) => {
